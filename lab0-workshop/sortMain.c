813726f9da61a2cdbadf76fdf43598b1ca1dcd57
nandish-jha <nandish-jha@gmail.com>
2025-09-20 21:54:00 -0600

    Quickly Added debugging tools...

---
+++ lab0-workshop/sortMain.c
@@ -0,0 +1,60 @@
+/*
+NANDISH JHA NAJ474 11282001
+*/
+
+#include <qsort.h>
+
+enum sType 
+  {
+    STRING, INTEGER, DOUBLE, COMPLEX
+  };
+
+
+#define MAXLINES 100
+char *lineptr[MAXLINES];
+
+
+int main(int argc, char *argv[])
+{
+  int nlines;
+  enum sType sortType = STRING;
+  int (*comparing)(void*, void*);
+  
+
+  if (argc != 2)
+    {
+      perror("Usage: perror. wrong number of arguments");
+      return -1;
+    }
+
+  sortType = atoi(argv[1]);
+
+
+  if (sortType == STRING) comparing = (Comparator) strcmp;
+  if (sortType == INTEGER) comparing = compareInt;
+  if (sortType == DOUBLE) comparing = compareDouble;
+  if (sortType == COMPLEX) comparing = compareComplex; 
+
+
+  
+  if ((nlines = readlines(lineptr, MAXLINES)) >=0)
+    {
+      printf("UNSORTED ORDER\n");
+      writelines(lineptr, nlines);
+      
+      myQsort ((void **)lineptr, 0, nlines-1, comparing);
+
+      printf("\nSORTED ORDER \n");
+      
+      writelines(lineptr, nlines);
+      return 0;
+    }
+  else
+    {
+      perror("problem with input");
+      return 1;
+    }
+}
+
+
+  
